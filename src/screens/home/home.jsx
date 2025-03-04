683c9747bf232fb280ed78bdce4488d0d973be6e
Gabriel Ruas <gabrielruas30@gmail.com>
2025-03-04 13:12:55 -0300

    Aula 01

---
+++ src/screens/home/home.jsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+
+function Home() {
+    return (
+        <>
+            <Text style={{ fontSize: 50 }}>Logo TaxiCar</Text>
+        </>
+    );
+}
+
+export default Home;
+
+
